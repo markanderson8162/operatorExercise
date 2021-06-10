7107c8ad305c3e43c7ca27fddd4b1a7fb1b00f21
Mark Anderson <markanderson8162@gmail.com>
2021-06-10 13:21:29 -0500

    created operator exercise

---
+++ operatorsExercise/operatorsExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace operatorsExercise
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a = 17;
+			int b = 4;
+			int quotent = a / b;
+			int remainder = a % b;
+
+			if(a == 17 && b == 4)
+			{
+				Console.WriteLine($"The answer is {quotent} with a remainder of {remainder}.");
+
+
+			}
+		}
+	}
+}
